2e82a90e2f1be365f1adca9193b69d55b8c73ebc
“amarasagekar” <amarasagekar@gmail.com>
2024-04-19 22:11:46 +0530

    Search Header on Home screen

--- Movie_App/src/screens/HomeScreen.tsx
+++ Movie_App/src/screens/HomeScreen.tsx
@@ -1,23 +1,92 @@
-import * as React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import {COLORS} from '../theme/theme';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  ActivityIndicator,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+import {COLORS, SPACING} from '../theme/theme';
+import {
+  upcomingMovies,
+  nowPlayingMovies,
+  popularMovies,
+  baseImagePath,
+} from '../api/apicalls';
+
+import InputHeader from '../components/InputHeader';
+
+const {width, height} = Dimensions.get('window');
 
 const HomeScreen = ({navigation}: any) => {
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] =
+    useState<any>(undefined);
+
+  const [popularMoviewList, setPopularMoviewList] = useState<any>(undefined);
+
+  const [upcomingMovieList, setUpcomingMovieList] = useState<any>(undefined);
+  const searchMoviesFunction = () => {
+    navigation.navigate('Search');
+  };
+
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviewList == undefined &&
+    popularMoviewList == null &&
+    upcomingMovieList == undefined &&
+    upcomingMovieList == null
+  ) {
+    return (
+      <ScrollView
+        style={styles.container}
+        bounces={false}
+        contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden />
+
+        <View style={styles.InputHeaderContainer}>
+          <InputHeader searchFunction={searchMoviesFunction} />
+        </View>
+
+        <View style={styles.loadinContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.push('MovieDetails');
-        }}>
-        <Text>HomeScreen</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView
+      style={styles.container}
+      bounces={false}
+      contentContainerStyle={styles.scrollViewContainer}>
+      <StatusBar hidden />
+
+      <View style={styles.InputHeaderContainer}>
+        <InputHeader searchFunction={searchMoviesFunction} />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     backgroundColor: COLORS.Black,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadinContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  InputHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
   },
 });
 

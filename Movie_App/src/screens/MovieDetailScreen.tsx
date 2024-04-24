8a1f594e4e7549aaecab3dd82f1489013ee5ec67
“amarasagekar” <amarasagekar@gmail.com>
2024-04-24 21:45:20 +0530

    implemented API to get movie detail and cast detail

--- Movie_App/src/screens/MovieDetailScreen.tsx
+++ Movie_App/src/screens/MovieDetailScreen.tsx
@@ -1,10 +1,58 @@
-import React, {useState} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
+import {movieDetails, movieCastDetails} from '../api/apicalls';
+
+const getMovieDetails = async (movieid: number) => {
+  try {
+    let response = await fetch(movieDetails(movieid));
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error('Somthing went wrong in getMovieDetails function ', error);
+  }
+};
+
+const getMovieCastDetails = async (movieid: number) => {
+  try {
+    let response = await fetch(movieCastDetails(movieid));
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(
+      'Somthing went wrong in getMovieCastDetails function ',
+      error,
+    );
+  }
+};
 
 const MovieDetailsScreen = ({navigation, route}: any) => {
   const [movieData, setMovieData] = useState(undefined);
   const [movieCastData, setMovieCastData] = useState<any>(undefined);
 
+  useEffect(() => {
+    (async () => {
+      const tempMovieData = await getMovieDetails(route.params.movieid);
+      setMovieData(tempMovieData);
+    })();
+
+    (async () => {
+      const tempMovieCastData = await getMovieCastDetails(route.params.movieid);
+      setMovieData(tempMovieCastData);
+    })();
+  }, []);
+
+  if (
+    movieData == undefined &&
+    movieData == null &&
+    movieCastData == undefined &&
+    movieCastData == null
+  ) {
+    return (
+      <ScrollView>
+        <View></View>
+      </ScrollView>
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>MovieDetailsScreen</Text>

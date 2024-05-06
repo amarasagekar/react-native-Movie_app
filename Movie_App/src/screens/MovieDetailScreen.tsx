4df4341e11fd4280b29be740015e6b6380a150a2
“amarasagekar” <amarasagekar@gmail.com>
2024-05-06 23:32:13 +0530

    movie detail, genre, poster image UI

--- Movie_App/src/screens/MovieDetailScreen.tsx
+++ Movie_App/src/screens/MovieDetailScreen.tsx
@@ -5,10 +5,22 @@ import {
   StyleSheet,
   ScrollView,
   ActivityIndicator,
+  StatusBar,
+  ImageBackground,
+  Image,
 } from 'react-native';
-import {movieDetails, movieCastDetails} from '../api/apicalls';
-import {COLORS, SPACING} from '../theme/theme';
+import {movieDetails, movieCastDetails, baseImagePath} from '../api/apicalls';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 import AppHeader from '../components/AppHeader';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import CustomIcon from '../components/CustomIcon';
 
 const getMovieDetails = async (movieid: number) => {
   try {
@@ -45,7 +57,7 @@ const MovieDetailsScreen = ({navigation, route}: any) => {
 
     (async () => {
       const tempMovieCastData = await getMovieCastDetails(route.params.movieid);
-      setMovieData(tempMovieCastData);
+      setMovieCastData(tempMovieCastData);
     })();
   }, []);
 
@@ -79,15 +91,50 @@ const MovieDetailsScreen = ({navigation, route}: any) => {
       style={styles.container}
       bounces={false}
       showsVerticalScrollIndicator={false}>
-      <View style={styles.appHeaderContainer}>
-        <AppHeader
-          name="close"
-          header={' '}
-          action={() => navigation.goBack()}
+      <StatusBar hidden />
+
+      <View>
+        <ImageBackground
+          source={{
+            uri: baseImagePath('w780', movieData?.backdrop_path),
+          }}
+          style={styles.imageBgG}>
+          <LinearGradient
+            colors={[COLORS.BlackRGB10, COLORS.Black]}
+            style={styles.linearGradient}>
+            <AppHeader
+              name="close"
+              header={''}
+              action={() => navigation.goBack()}
+            />
+          </LinearGradient>
+        </ImageBackground>
+        <View style={styles.imageBgG} />
+        <Image
+          source={{uri: baseImagePath('w342', movieData?.poster_path)}}
+          style={styles.cardImage}
         />
       </View>
-      <View style={styles.loadinContainer}>
-        <ActivityIndicator size={'large'} color={COLORS.Orange} />
+
+      <View>
+        <CustomIcon name="clock" style={styles.clockIcon} />
+        <Text style={styles.runtimeText}>
+          {Math.floor(movieData?.runtime / 60)}h{' '}
+          {Math.floor(movieData?.runtime % 60)}m
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.title}> {movieData?.original_title}</Text>
+        <View style={styles.genreContainer}>
+          {movieData?.genres.map((item: any) => {
+            return (
+              <View style={styles.genreBox} key={item.id}>
+                <Text style={styles.genreText}>{item.name}</Text>
+              </View>
+            );
+          })}
+        </View>
+        <Text style={styles.tagline}>{movieData?.tagline}</Text>
       </View>
     </ScrollView>
   );
@@ -110,6 +157,73 @@ const styles = StyleSheet.create({
   appHeaderContainer: {
     marginHorizontal: SPACING.space_36,
     marginTop: SPACING.space_20 * 2,
+  },
+  imageBgG: {
+    width: '100%',
+    aspectRatio: 3072 / 1727,
+  },
+  linearGradient: {
+    height: '100%',
+  },
+  cardImage: {
+    width: '60%',
+    aspectRatio: 200 / 300,
+    position: 'absolute',
+    bottom: 0,
+    alignSelf: 'center',
+  },
+  clockIcon: {
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.WhiteRGBA50,
+    marginRight: SPACING.space_8,
+  },
+  timeContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: SPACING.space_15,
+  },
+  runtimeText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  title: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+    marginHorizontal: SPACING.space_36,
+    marginVertical: SPACING.space_15,
+    textAlign: 'center',
+  },
+  genreContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: SPACING.space_20,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  genreBox: {
+    borderColor: COLORS.WhiteRGBA50,
+    borderWidth: 1,
+    paddingHorizontal: SPACING.space_10,
+    paddingVertical: SPACING.space_4,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  genreText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.WhiteRGBA75,
+  },
+  tagline: {
+    fontFamily: FONTFAMILY.poppins_thin,
+    fontSize: FONTSIZE.size_14,
+    fontStyle: 'italic',
+    color: COLORS.White,
+    marginHorizontal: SPACING.space_36,
+    marginVertical: SPACING.space_15,
+    textAlign: 'center',
   },
 });
 

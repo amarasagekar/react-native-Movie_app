43a362920b9c3756180e77c5ca3b1e0bb4328615
“amarasagekar” <amarasagekar@gmail.com>
2024-05-12 23:45:57 +0530

    Added hooks

--- Movie_App/src/screens/SeatBookingScreen.tsx
+++ Movie_App/src/screens/SeatBookingScreen.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, ScrollView, StatusBar} from 'react-native';
+import {COLORS} from '../theme/theme';
 
 const timeArray: string[] = [
   '10:30',
@@ -10,7 +11,7 @@ const timeArray: string[] = [
   '21:00',
 ];
 
-const generateDate = () => {
+const generateDate = (): string[] => {
   const date = new Date();
   let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let weekdays = [];
@@ -23,18 +24,65 @@ const generateDate = () => {
     };
     weekdays.push(tempDate);
   }
+  return weekdays;
 };
-const SeatBookingScreen = () => {
-  generateDate();
+
+const generateSeats = () => {
+  let numRow = 8;
+  let numColumn = 3;
+  let rowArray = [];
+  let start = 1;
+  let reachnine = false;
+
+  for (let i = 0; i < numRow; i++) {
+    let columnArray = [];
+    for (let j = 0; j < numColumn; j++) {
+      let seatObject = {
+        number: start,
+        taken: Boolean(Math.round(Math.random())),
+        selected: false,
+      };
+      columnArray.push(seatObject);
+      start++;
+    }
+    if (i == 3) {
+      numColumn += 2;
+    }
+    if (numColumn < 9 && !reachnine) {
+      numColumn += 2;
+    } else {
+      reachnine = true;
+      numColumn -= 2;
+    }
+    rowArray.push(columnArray);
+  }
+  return rowArray;
+};
+
+const SeatBookingScreen = ({navigation, route}: any) => {
+  const [dateArray, setDateArray] = useState<string[]>(generateDate());
+  const [selectedDateIndex, setSelectedDateIndex] = useState<any>();
+  const [price, setPrice] = useState<number>(0);
+  const [twoDSeatArray, setTwoDSetArray] = useState<any[][]>(generateSeats());
+  const [selectedSeatArray, setSelectedSeatArray] = useState([]);
+  const [selectedTimeIndex, setSelectedTimeIndex] = useState<any>();
+
   return (
-    <View style={styles.container}>
-      <Text>SeatBookingScreen</Text>
-    </View>
+    <ScrollView
+      style={styles.container}
+      bounces={false}
+      showsVerticalScrollIndicator={false}>
+      <StatusBar hidden />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
 });
 
 export default SeatBookingScreen;
